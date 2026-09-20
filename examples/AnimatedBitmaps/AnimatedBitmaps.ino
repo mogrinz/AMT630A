51b37596152ff0fd7b512a4274ef2768f964c242
Mike Ogrinz <mike@ogrinz.com>
2026-09-20 15:41:42 -0400

    Add source headers and bundle robot bitmap

--- examples/AnimatedBitmaps/AnimatedBitmaps.ino
+++ examples/AnimatedBitmaps/AnimatedBitmaps.ino
@@ -1,3 +1,14 @@
+/*
+Library Name: AMT630A On-Screen-Display (OSD)
+Created by: Michael Ogrinz, September 2026
+Contact: mike@ogrinz.com, https://youtube.com/@ogrinz_labs
+Description: A library for creating on-screen text and bitmap graphics over a live composite video signal for devices using the AMT630A video decoder and digital TFT-LCD panel-control SoC.
+Repository: https://github.com/mogrinz/AMT630A
+License: MIT License
+Changelog:
+v0.7.0 - 2026-09-20: Initial release.
+*/
+
 #include <AMT630A_OSD.h>
 #include "robot.h"
 
@@ -27,7 +38,7 @@ AMT630A_OSD::BitmapHandle robotHandle;
   synchronize bitmap-window display updates differently from normal text
   windows.
 
-  Put your generated 64x64 robot.h in the same Arduino sketch folder.
+  The bundled robot.h provides the 64x64 robot bitmap used by this example.
 
   Serial Monitor: 115200 baud
 */
